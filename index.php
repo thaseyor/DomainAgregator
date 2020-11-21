4d863178dd7f0fecc81ae543a482f8b7b8849875
thaseyor <56724794+thaseyor@users.noreply.github.com>
2020-11-21 12:07:25 +0600

    main features

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <head>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+<link rel="stylesheet" href="style.css">
 </head>
 <?php
-$dir    = '../';
+$dir    = '../../';
 $files = scandir($dir);
 unset($files[0]);
 unset($files[1]);
-print_r($files1);
+echo '
+<div class="level container vertical-center" style="position:relative;width:25%">
+<div class="level-left">Wow.</div>
+<div class="level-right"><ul>';
+foreach($files as $value){
+echo "<li><a href='https://$value'>$value</a></li>";
+}
+echo '</ul></div></div>';
 ?>
